e8103e149901ed393da9ff2ad0cf7f118fb48f81
Innocent Anozie <iygeal@gmail.com>
2025-05-15 10:00:08 +0100

    Created the wallet schema with references to a user schema

--- models/wallet.js
+++ models/wallet.js
@@ -1,16 +1,25 @@
-// Import mongoose to create the wallet schema
+// Import mongoose to define the Wallet schema
 const mongoose = require('mongoose');
 
-const walletSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+// Define wallet schema
+const walletSchema = new mongoose.Schema(
+  {
+    // Reference to the user who owns this wallet
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // Links to a User document
+      ref: 'User',
+      required: true,
+    },
+    // Wallet balance (defaults to 0 when a new wallet is created)
+    balance: {
+      type: Number,
+      default: 0,
+    },
   },
-  balance: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
+// Export wallet model as Wallet
 module.exports = mongoose.model('Wallet', walletSchema);
