a09504f1993c8dd41effb955deb65933591b4471
Innocent Anozie <iygeal@gmail.com>
2025-05-24 11:34:58 +0100

    UPDATE: Updated the user schema to include fields for password reset.

--- models/user.js
+++ models/user.js
@@ -27,6 +27,12 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Wallet',
     required: true
+  },
+  resetToken: {
+    type: String
+  },
+  resetTokenExpiry: {
+    type: Date
   }
 },
 {
