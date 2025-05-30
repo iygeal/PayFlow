5614e9c5617ea192cb8777180856e1c66fdc675a
Innocent Anozie <iygeal@gmail.com>
2025-05-30 22:07:14 +0100

    UPDATED: Added controller functions to handle user update and account deletion.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,19 @@ const router = express.Router();
 const authorize = require('../middleware/authMiddleware');
 
 // Import controller functions
-const { getUserProfile } = require('../controllers/userController');
+const {
+  getUserProfile,
+  updateUserProfile,
+  deleteUserProfile,
+} = require('../controllers/userController');
 
 // Route to get the profile of the logged-in user
 router.get('/profile/:id', authorize, getUserProfile);
+
+// Route to update the profile of the logged-in user
+router.put('/profile/:id', authorize, updateUserProfile);
+
+// Route to delete the logged-in user's account
+router.delete('/profile/:id', authorize, deleteUserProfile);
 
 module.exports = router;
